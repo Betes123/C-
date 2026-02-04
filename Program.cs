e0e14d4a7d3d3a30fac294b0b04a0ffa764a4bed
Betes123 <nguyentaidinh08@gmail.com>
2026-02-04 14:44:58 +0700

    làm bài 1.1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace TinhTienDien
+{
+    class Program
+    {
+        static void Main()
+        {
+            double tien= KWH.TinhToan();
+            Console.WriteLine("tien dien la: "+tien);
+        }
+    }
+}
--- bai1.cs
+++ bai1.cs
@@ -1,2 +1,42 @@
-Console.WriteLine("xin chao!");
-Console.WriteLine("xin chao!");
+using System;
+
+namespace TinhTienDien
+{
+    class KWH
+    {
+        const int muc1=100, muc2=150,muc3=300;
+        const int gia1=2000, gia2=2500, gia3=3000;
+        public static double TinhToan()
+        {
+            Console.WriteLine("nhap kwh de tinh tien dien: ");
+            int kwh=Convert.ToInt32(Console.ReadLine());
+        
+            double sotien=0;
+            if (kwh <= muc1)
+            {
+                sotien=kwh*gia1;
+            }
+            else
+            {
+                if (kwh < muc2)
+                {
+                    sotien=(muc1*gia1) + (kwh-muc1)*gia2;
+                    //130 = 100*2000      130-100=30*2500
+                }
+                else
+                {
+                    if (kwh > muc2)
+                    {
+                        sotien=(muc1*gia1) + (muc2-muc1)*gia2 + (kwh - muc2)*gia3;
+                        // 151  100*2000      150-100=50*2500 +  151-150=1*3000     
+                    }
+                }
+            }
+            if(kwh > muc3)
+            {
+                sotien*=1.1;
+            }
+            return sotien;
+        }
+    }
+}
